c4b06c231489e8f881501871197d53f761ed1db8
Zefir <56188831+Kerozov@users.noreply.github.com>
2023-09-26 01:00:53 +0300

    Add configuration and DI

---
+++ Core/Application/Helpers/ConfigurationApplicationLayer.cs
@@ -0,0 +1,16 @@
+using Application.Models.EssayModels.Interfaces;
+using Application.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application.Helpers;
+
+public static class ConfigurationApplicationLayer
+{
+    public static IServiceCollection AddConfigurationApplicationLayer(this IServiceCollection services)
+    {
+        services.AddScoped<IEssayService, EssayService>();
+       
+        return services;
+
+    }
+}
